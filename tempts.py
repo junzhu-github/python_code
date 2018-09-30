31ce6ee50b238b4d8317f69ce1f0a1a6b10c75fe
junzhu-github <junzhu-github>
2018-09-30 16:57:41 +0800

    happy 10.1

--- tempts.py
+++ tempts.py
@@ -6477,23 +6477,136 @@ from mpl_toolkits.mplot3d import Axes3D
 # print(vague_list[2])
 # print(vague_list[2])
 
-class Queue:
-  def __init__(self, contents):
-    self._hiddenlist = list(contents)
+# class Queue:
+#   def __init__(self, contents):
+#     self._hiddenlist = list(contents)
 
-  def push(self, value):
-    self._hiddenlist.insert(0, value)
+#   def push(self, value):
+#     self._hiddenlist.insert(0, value)
    
-  def pop(self):
-    return self._hiddenlist.pop(-1)
+#   def pop(self):
+#     return self._hiddenlist.pop(-1)
 
-  def __repr__(self):
-    return "Queue({})".format(self._hiddenlist)
+#   def __repr__(self):
+#     return "Queue({})".format(self._hiddenlist)
 
-queue = Queue([1, 2, 3])
-print(queue)
-queue.push(0)
-print(queue)
-queue.pop()
-print(queue)
-print(queue._hiddenlist)
+# queue = Queue([1, 2, 3])
+# print(queue)
+# queue.push(0)
+# print(queue)
+# queue.pop()
+# print(queue)
+# print(queue._hiddenlist)
+
+# class Spam:
+#   __egg = 7
+#   def print_egg(self):
+#     print(self.__egg)
+
+# s = Spam()
+# s.print_egg()
+# print(s._Spam__egg)
+# print(s.__egg)
+
+# class Rectangle:
+#   def __init__(self, width, height):
+#     self.width = width
+#     self.height = height
+
+#   def calculate_area(self):
+#     return self.width * self.height
+
+#   @classmethod
+#   def new_square(cls, side_length):
+#     return cls(side_length, side_length)
+
+# square = Rectangle.new_square(5)
+# print(square.calculate_area())
+
+# class Pizza:
+#   def __init__(self, toppings):
+#     self.toppings = toppings
+
+#   @staticmethod
+#   def validate_topping(topping):
+#     if topping == "pineapple":
+#       raise ValueError("No pineapples!")
+#     else:
+#       return True
+
+# ingredients = ["cheese", "onions", "spam"]
+# if all(Pizza.validate_topping(i) for i in ingredients):
+#   pizza = Pizza(ingredients) 
+
+# class Pizza:
+#   def __init__(self, toppings):
+#     self.toppings = toppings
+
+#   @staticmethod
+#   def validate_topping(topping):
+#     if topping == "pineapple":
+#       raise ValueError("No pineapples!")
+#       print('bug')
+#     else:
+#       return True
+
+# ingredients = ["cheese", "onions", "spam","pineapple"]
+# if all(Pizza.validate_topping(i) for i in ingredients):
+#   pizza = Pizza(ingredients) 
+
+# class Pizza:
+#   def __init__(self, toppings):
+#     self.toppings = toppings
+#     self._pineapple_allowed = False
+
+#   @property
+#   def pineapple_allowed(self):
+#     return self._pineapple_allowed
+
+#   @pineapple_allowed.setter
+#   def pineapple_allowed(self, value):
+#     if value:
+#       password = input("Enter the password: ")
+#       if password == "Sw0rdf1sh!":
+#         self._pineapple_allowed = value
+#       else:
+#         raise ValueError("Alert! Intruder!")
+
+# pizza = Pizza(["cheese", "tomato"])
+# print(pizza.pineapple_allowed)
+# pizza.pineapple_allowed = True
+# print(pizza.pineapple_allowed)
+
+# def get_input():
+#   command = input(": ").split()
+#   verb_word = command[0]
+#   if verb_word in verb_dict:
+#     verb = verb_dict[verb_word]
+#   else:
+#     print("Unknown verb {}". format(verb_word))
+#     return
+
+#   if len(command) >= 2:
+#     noun_word = command[1]
+#     print (verb(noun_word))
+#   else:
+#     print(verb("nothing"))
+
+# def say(noun):
+#   return 'You said "{}"'.format(noun)
+
+# verb_dict = {
+#   "say": say,
+# }
+
+# while True:
+#   get_input()
+
+def use_logging(func):
+    logging.warn("%s is running" % func.__name__)
+    func()
+
+def foo():
+    print('i am foo')
+
+use_logging(foo)
